4d15e7e771fe6700a845c2ae9c5fafb0bf39cb13
rolsconsult <rolsconsult4@gmail.com>
2023-01-24 11:33:03 +0000

    Initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("I am Rolemodel");
+        System.out.println("I am the best programmer out there");
+        System.out.println("You can also become the best programmer possible");
+    }
+}
